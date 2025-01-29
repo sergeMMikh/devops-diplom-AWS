be183c794dc99ecc1e2e43f1b8ee6fa1fbafd54b
smmih <s.m.mih@mail.ru>
2025-01-29 23:35:29 +0000

    modified:   bootstrap/modules/s3/main.tf new file:   bootstrap/tfplan-bootstrap new file:   deploy.sh

--- bootstrap/modules/s3/main.tf
+++ bootstrap/modules/s3/main.tf
@@ -27,14 +27,14 @@ resource "aws_s3_bucket_public_access_block" "backend_dd_block" {
 }
 
 
-resource "aws_s3_object" "image" {
-  bucket                 = aws_s3_bucket.backend_dd.id
-  key                    = "cafe.jpg"
-  source                 = "images/cafe.jpg"
-  server_side_encryption = "AES256"
+# resource "aws_s3_object" "image" {
+#   bucket                 = aws_s3_bucket.backend_dd.id
+#   key                    = "cafe.jpg"
+#   source                 = "images/cafe.jpg"
+#   server_side_encryption = "AES256"
 
-  depends_on = [aws_s3_bucket.backend_dd]
-}
+#   depends_on = [aws_s3_bucket.backend_dd]
+# }
 
 # Политика доступа для публичного чтения ... пока не нужна
 # resource "aws_s3_bucket_policy" "backend_dd_policy" {
