4323bd377aecd55668e1fbd5f5fa7ab9705b16fc
smmih <s.m.mih@mail.ru>
2025-02-04 20:42:11 +0000

    modified:   terraform/infrastructure/modules/security_group/main.tf new file:   "\320\274\320\265\320\273\320\276\321\207\320\270/spray.txt"

--- terraform/infrastructure/modules/security_group/main.tf
+++ terraform/infrastructure/modules/security_group/main.tf
@@ -1,14 +1,21 @@
 resource "aws_security_group" "sg_4dd" {
   vpc_id = var.vpc_id
 
-  dynamic "ingress" {
-    for_each = var.allowed_ingress_ports
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = var.allowed_cidr_blocks
-    }
+  # dynamic "ingress" {
+  #   for_each = var.allowed_ingress_ports
+  #   content {
+  #     from_port   = ingress.value
+  #     to_port     = ingress.value
+  #     protocol    = "tcp"
+  #     cidr_blocks = var.allowed_cidr_blocks
+  #   }
+  # }
+  # Разрешаем ВСЕ входящие подключения
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.allowed_cidr_blocks
   }
 
   ingress {
